391564fb15333b38ca8bb9cd570676955f0afd9f
Mikkel Luja Rasmussen <mlr@redbarnet.dk>
2021-03-06 03:28:40 +0100

    Init

---
+++ FLogic.CLI/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    #if DEBUG    
+    printfn "Arguments : %A" argv.Length
+    if argv.Length > 0 then printfn "All args : %A" (Array.toList argv)
+    #endif
+    //No args provided or contains overwrite keyword interactive
+    let input = argv.ToString()
+    if argv.Length = 0 || input.Contains "-i" || input.Contains "--interactive" then 
+        printfn "Welcome to FLogic.Interactive!"
+        else 
+        printfn "Welcome to FLogic.CLI!"
+
+    let rs = Console.ReadLine()
+    
+    printf "%A" rs
+    0 // return an integer exit code
+
+    // Parse input
+    // FLogic Interactive
+
+    // Functionailty
+        //Type Simplify
+        //Gate Eval
+        //Tree
+        //Truth Table
---
+++ FLogic/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace FLogic
+
+open System
+
+module Say =
+    let hello (name:string) =
+        let msg = sprintf "Hello %s" name
+        printfn "%A" msg
+        msg
