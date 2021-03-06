e0c66968b6cef97b46bfd2d2f955fa1f4e97e1d8
Mikkel Luja Rasmussen <mlr@redbarnet.dk>
2021-03-06 04:49:54 +0100

    Converted all projects to be .net framework for better workflow

--- FLogic.CLI/Program.fs
+++ FLogic.CLI/Program.fs
@@ -2,22 +2,71 @@
 
 open System
 
+let rec menu() : Option<string> =
+    printfn "Menu"
+    let rs = Console.ReadLine()
+    let action = 
+        match rs with
+        | x when x = "Simplify" -> 
+            printf "Simp"
+            None
+        | x when x = "exit" -> 
+            Some("Exit")
+        | _ -> 
+            printf "Invalid command"
+            Some("Error")
+    if action.IsSome 
+    then Some("Exit")
+    else menu()
+
+let rec any str1 str2 = function
+    | x::xs -> 
+        if str1 = x || str2 = x
+        then true
+        else any str1 str2 xs
+    | _ -> false
+
 [<EntryPoint>]
 let main argv =
+    let lst_args = (Array.toList argv)
     #if DEBUG    
     printfn "Arguments : %A" argv.Length
-    if argv.Length > 0 then printfn "All args : %A" (Array.toList argv)
+    if argv.Length > 0 then printfn "All args : %A" lst_args
     #endif
-    //No args provided or contains overwrite keyword interactive
-    let input = argv.ToString()
-    if argv.Length = 0 || input.Contains "-i" || input.Contains "--interactive" then 
-        printfn "Welcome to FLogic.Interactive!"
-        else 
-        printfn "Welcome to FLogic.CLI!"
 
-    let rs = Console.ReadLine()
-    
-    printf "%A" rs
+    let e_arg str1 str2 = any str1 str2 lst_args 
+
+    //No args provided or contains overwrite keyword interactive
+    if argv.Length = 0 || (any "-i" "--interactive" lst_args) then 
+        printfn "Welcome to FLogic.Interactive!"
+        let res = menu()
+        if res.IsSome then exit(0)
+    elif (e_arg "-h" "--help") then
+        printfn "Long help message"
+    else 
+        printfn "Welcome to FLogic.CLI!"
+        if e_arg "-tt" "--truthtable" then printfn "With truthtable"
+        if e_arg "-t" "--tree" then printfn "With tree"
+
+        let gateEval = e_arg "-ge" "--gateeval"
+        let typeSimp = e_arg "-ts" "--typesimp"
+
+        if gateEval && typeSimp then
+            printfn "Invalid request, you can't both do gate evaluation and type simplification"
+        elif gateEval then 
+            printfn "Gate evaluation"
+            if not (e_arg "-in" "--input") 
+            then printfn "No input provided"
+            else printfn "Reading input"
+
+        elif typeSimp then
+            printfn "Type simplification"
+        else 
+            printfn "Unknown command, exiting"
+            
+    printfn ""
+    printfn "Press any key to exit"
+    let _ = Console.ReadLine()    
     0 // return an integer exit code
 
     // Parse input
---
+++ FLogic.GUI/Program.fs
@@ -0,0 +1,31 @@
+﻿open System.Drawing
+open System.Windows.Forms
+open System
+
+let form = new Form(Width = 400, Height = 400, Text = "draw test")
+let panel = new FlowLayoutPanel()
+form.Controls.Add(panel)
+
+let paint(e : PaintEventArgs) =
+    let pen = new Pen(Color.Black);  
+    e.Graphics.DrawLine(pen, new PointF(100.0f, 100.0f), new PointF(200.0f, 200.0f))
+
+let button = new Button()
+button.Text <- "Click to draw"
+button.AutoSize <- true
+
+//button.Click.Add(fun _ -> form.Paint.Add(paint)) // <- does not draw a line on click
+
+//This works
+button.Click.Add(fun _ -> form.Paint.Add(paint); form.Invalidate()) 
+
+panel.Controls.Add(button)
+
+//form.Paint.Add(paint) //<- here, if uncommented, it will draw a line when the script is run
+form.Show()
+
+[<EntryPoint>]
+let main argv= 
+    printfn "Running F# App"
+    System.Windows.Forms.Application.Run(form)
+    0
