0b46b27d3817f1c9d6d9f92833435955c9ea70d0
Mikkel Luja Rasmussen <milr@itu.dk>
2021-03-06 15:24:40 +0100

    Added IO Test for parsing files

--- FLogic.Tests/IOTests.fs
+++ FLogic.Tests/IOTests.fs
@@ -26,3 +26,23 @@ let ``Binary string write test`` () =
     printfn "%A" msg
     File.Delete(name)
     Assert.Equal(content,msg)
+
+[<Fact>]
+let ``Binary string write json no data loss test`` () =
+    let name = "jsonwritetest.js"
+    let content = """{
+    "name" : "Scott",
+    "isMale" : true,
+    "bday" : {"year":2001, "month":12, "day":25 },
+    "favouriteColors" : ["blue", "green"],
+    "emptyArray" : [],
+    "emptyObject" : {}
+    }"""
+    let res1 = ParserLibrary.run FLogic.lValue content |> ParserLibrary.sprintResult
+    File.WriteAllText(name,content)
+    let msg = File.ReadAllText(name)
+    let res2 = ParserLibrary.run FLogic.lValue msg |> ParserLibrary.sprintResult
+    printfn "%A" msg
+    File.Delete(name)
+    Assert.Equal(res1,res2)
+    Assert.Equal(content,msg)
