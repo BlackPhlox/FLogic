604fd068633f0e483f9752021e007405409dd23e
Mikkel Luja Rasmussen <milr@itu.dk>
2021-03-06 11:55:30 +0100

    Added parser lib

---
+++ FLogic/FLogicParser.fs
@@ -0,0 +1,19 @@
+﻿open System
+open ParserLibrary
+
+
+type JValue =
+    | JString of string
+    | JNumber of float
+    | JBool   of bool
+    | JNull
+    | JObject of Map<string, JValue>
+    | JArray  of JValue list
+
+  let jNull =
+      pstring "null"
+      |>> (fun _ -> JNull)  // map to JNull
+      <?> "null"            // give it a labellet jNull =
+      pstring "null"
+      |>> (fun _ -> JNull)  // map to JNull
+      <?> "null"            // give it a label
