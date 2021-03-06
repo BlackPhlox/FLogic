a14b912b49cef0b2db9a7db5580341849425923f
Mikkel Luja Rasmussen <milr@itu.dk>
2021-03-06 10:33:47 +0100

    Added simple read/write test cases

--- FLogic.Tests/Tests.fs
+++ FLogic.Tests/Tests.fs
@@ -3,7 +3,32 @@ module Tests
 open System
 open Xunit
 open FLogic
+open System.IO
 
 [<Fact>]
 let ``My test`` () =
     Assert.Equal("Hello Test", Say.hello "Test")
+
+[<Fact>]
+let ``My binary write test`` () =
+    let v = 42
+    let name = "mybinarytestdata"
+    let bw = new BinaryWriter(new FileStream(name, FileMode.Create))
+    bw.Write(v);
+    bw.Close();
+    let br = new BinaryReader(new FileStream(name, FileMode.Open));
+    let i = br.ReadInt32();
+    printfn "%A" i
+    br.Close();
+    File.Delete(name)
+    Assert.Equal(v,i)
+
+[<Fact>]
+let ``My binary string write test`` () =
+    let name = "mystringtestdata"
+    let content = "hello\nworld"
+    File.WriteAllText(name,content)
+    let msg = File.ReadAllText(name)
+    printfn "%A" msg
+    File.Delete(name)
+    Assert.Equal(content,msg)
