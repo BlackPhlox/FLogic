f9add8e790f42b2725f3e374f777f37f450c62d1
Mikkel Luja Rasmussen <mlr@redbarnet.dk>
2021-04-25 12:31:47 +0200

    Added base and simple test cases

--- FLogic.Tests/Tests.fs
+++ FLogic.Tests/Tests.fs
@@ -7,3 +7,81 @@ open FLogic
 [<Fact>]
 let ``My test`` () =
     Assert.Equal("Hello Test", Say.hello "Test")
+
+[<Fact>]
+let ``Constuct base test`` () =
+    Assert.Equal(FL_Base.T "Test", FL_Base.T "Test")
+
+[<Fact>]
+let ``Simple calculation test fail`` () =
+    Assert.Equal(FL_Base.F, 
+        Say.calc (
+            FL_Base.G(
+                FL_Base.AND(
+                    FL_Base.T "Test", 
+                    FL_Base.F
+                )
+            )
+        )
+    )
+
+[<Fact>]
+let ``Simple calculation test success`` () =
+    Assert.Equal(FL_Base.T "Test", 
+        Say.calc (
+            FL_Base.G(
+                FL_Base.AND(
+                    FL_Base.T "Test", 
+                    FL_Base.T "Test"
+                )
+            )
+        )
+    )
+
+[<Fact>]
+let ``Simple map calculation test success`` () =
+    Assert.Equal(FL_Base.T "Test", 
+        (
+            Say.calcm (
+                FL_Base.G(
+                    FL_Base.AND(
+                        FL_Base.T "Test", 
+                        FL_Base.V "H"
+                    )
+                )
+            )
+        (Map.ofList [("H","Test")])
+        )
+    )
+
+[<Fact>]
+let ``Simple type map calculation test success`` () =
+    Assert.Equal(FL_Base.T true, 
+        (
+            Say.calcm (
+                FL_Base.G(
+                    FL_Base.AND(
+                        FL_Base.T true, 
+                        FL_Base.V "H"
+                    )
+                )
+            )
+        (Map.ofList [("H",true)])
+        )
+    )
+
+[<Fact>]
+let ``Simple map calculation test fails`` () =
+    Assert.Equal(FL_Base.F, 
+        (
+            Say.calcm (
+                FL_Base.G(
+                    FL_Base.AND(
+                        FL_Base.T "Test", 
+                        FL_Base.V "HEllo"
+                    )
+                )
+            )
+        (Map.ofList [("H","Test")])
+        )
+    )
--- FLogic/Library.fs
+++ FLogic/Library.fs
@@ -2,8 +2,39 @@
 
 open System
 
+module FL_Base =
+    type FL<'a> = 
+    | T of 'a
+    | F
+    | O of string
+    | V of string
+    | G of G<'a>
+    
+    and G<'a> =
+    | AND of FL<'a> * FL<'a>
+
 module Say =
     let hello (name:string) =
         let msg = sprintf "Hello %s" name
         printfn "%A" msg
         msg
+
+    let constructBase str =
+        FL_Base.T str
+
+    let constructGT str =
+        FL_Base.T (FL_Base.T str)
+
+    let rec calcm (gts: FL_Base.FL<'a>) (m: Map<string, 'a>): FL_Base.FL<'a> =
+        match gts with
+        | FL_Base.T x -> FL_Base.T x
+        | FL_Base.F -> FL_Base.F
+        | FL_Base.V v -> 
+            let res = Map.tryFind v m
+            if res.IsSome then FL_Base.T res.Value else FL_Base.F
+        | FL_Base.G g -> 
+            match g with
+            FL_Base.AND(x,y) -> if calcm x m = calcm y m then calcm x m else FL_Base.F
+
+    let rec calc (gts: FL_Base.FL<'a>) : FL_Base.FL<'a> =
+        calcm gts Map.empty
